ef796afe7642f1b2e895553c936300889fdfcc30
Reila W <takaii666@gmail.com>
2023-04-10 16:12:22 +0800

    useMemo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import styled from '@emotion/styled';
 
 import { ThemeProvider } from '@emotion/react';
 import dayjs from 'dayjs';
+import WeatherIcon from './components/WeatherIcon';
 
 // 載入圖示
-import { ReactComponent as DayCloudyIcon } from './images/day-cloudy.svg';
 import { ReactComponent as AirFlowIcon } from './images/airFlow.svg';
 import { ReactComponent as RainIcon } from './images/rain.svg';
 import { ReactComponent as RefreshIcon } from './images/refresh.svg';
@@ -165,9 +165,7 @@ const Refresh = styled.div`
   }
 `;
 
-const DayCloudy = styled(DayCloudyIcon)`
-  flex-basis: 30%;
-`
+
 
 
 
@@ -307,6 +305,7 @@ function App() {
     observationTime,
     isLoading,
     comfortability,
+    weatherCode,
   } = weatherElement;
 
   return (
@@ -325,7 +324,7 @@ function App() {
             <Temperature>
               {Math.round(temperature)}<Celsius>°C</Celsius>
             </Temperature>
-            <DayCloudy />
+            <WeatherIcon weatherCode={weatherCode} moment="night" />
           </CurrentWeather>
           <AirFlow>
             <AirFlowIcon />{windSpeed} m/h
